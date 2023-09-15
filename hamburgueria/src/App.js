eb13617907b7f91ea44c45ca7cbe610ac689adb2
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2023-09-15 10:56:41 -0300

    Update App.js
    Html da Navbar

--- hamburgueria/src/App.js
+++ hamburgueria/src/App.js
@@ -1,8 +1,20 @@
 import Combo from "./img/combo.png"
-
+Import Navlogo from "./img.logo2.png"
 function App() {
   return (
     <div className="Conteiner">
+      <div id="topo">
+        <div id="navbar">
+          <img src={Navlogo} id="imgnav"></img>
+          <ul>
+            <button>Delivery</button>
+            <button>Cupons</button>
+            <button>Menu</button>
+            <button>Restaurantes</button>
+            <button>Trabalhe Conosco</button>
+          </ul>
+        </div>
+      </div>
       <img className="combo" src={Combo} alt="Combo1" title="Combo1" />
       <div className="produtos">
         <h1>Cards Produtos</h1>
