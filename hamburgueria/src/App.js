66c3338d0231d2e6b63c73cf7b4cf2b3216fda24
Mourvim <108086840+Mourvim@users.noreply.github.com>
2023-09-15 14:03:00 -0300

    Slider
    Criação de um slider

--- hamburgueria/src/App.js
+++ hamburgueria/src/App.js
@@ -1,6 +1,25 @@
+import {register} from 'swiper/element/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Hero1 from "./img/Hero1.png"
 import Combo from "./img/combo.png"
+import Hero2 from "./img/Hero2.png"
+import Hero3 from "./img/Hero3.png"
 import Navlogo from "./img/logo2.png"
+
+register();
+
 function App() {
+const data = [
+  {id: '1', image: Combo},
+  {id: '2', image: Hero1},
+  {id: '3', image: Hero2},
+  {id: '4', image: Hero3}
+]
+
   return (
     <div className="Conteiner">
       <div className="topo">
@@ -15,7 +34,17 @@ function App() {
           </ul>
         </div>
       </div>
-      <img className="combo" src={Combo} alt="Combo1" title="Combo1" />
+      <Swiper
+        slidesPerView={1}
+        pagination={{clickable: true}}
+        navigation
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <img src={item.image} alt='slider' className='slide-item' />
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div className="produtos">
         <h1>Cards Produtos</h1>
       </div>
