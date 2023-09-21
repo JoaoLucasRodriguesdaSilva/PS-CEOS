727e7454e542713eeb554b776cf3190905983ee4
lucaspdc21 <lucaspdc2003@gmail.com>
2023-09-21 19:37:55 -0300

    alteração cards e banner

--- hamburgueria/src/App.js
+++ hamburgueria/src/App.js
@@ -6,29 +6,38 @@ import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import Hero1 from "./img/Hero1.png"
-import Combo from "./img/combo.png"
+/*import Combo from "./img/combo.png"*/
 import Hero2 from "./img/Hero2.png"
 import Hero3 from "./img/Hero3.png"
 import Navlogo from "./img/logo2.png"
 import CardSlider from './CardSlider'
 import './index.css'
 import React from 'react'
-import teste_lucas_card from "./img/mr mostarda.png"
+import cardapio1 from "./img/cardapio/REAL_MOSTARDA.png"
+import cardapio2 from "./img/cardapio/REAL_AMERICANO.png"
+import cardapio3 from "./img/cardapio/REAL_BACON.png"
+import cardapio4 from "./img/cardapio/REAL_CHICKEN.png"
+import cardapio5 from "./img/cardapio/REAL_DELUXE.png"
+import cardapio6 from "./img/cardapio/REAL_DUPLO.png"
+import cardapio7 from "./img/cardapio/REAL_WHOPPER.png"
+import cardapio8 from "./img/cardapio/SHAKE_CEOS.png"
+import banner from "./img/banner_semana_hamburguer.png"
 
 register();
 
 function App() {
 const data1 = [
-  { id: '1', image: teste_lucas_card },
-  { id: '2', image: teste_lucas_card },
-  { id: '3', image: teste_lucas_card },
-  { id: '4', image: teste_lucas_card },
-  { id: '5', image: teste_lucas_card },
-  { id: '6', image: teste_lucas_card },
-  { id: '7', image: teste_lucas_card },
+  { id: '1', image: cardapio1 },
+  { id: '2', image: cardapio2 },
+  { id: '3', image: cardapio3 },
+  { id: '4', image: cardapio4 },
+  { id: '5', image: cardapio5 },
+  { id: '6', image: cardapio6 },
+  { id: '7', image: cardapio7 },
+  { id: '8', image: cardapio8 }
 ]
 const data = [
-  {id: '1', image: Combo},
+  {id: '1', image: banner},
   {id: '2', image: Hero1},
   {id: '3', image: Hero2},
   {id: '4', image: Hero3}
